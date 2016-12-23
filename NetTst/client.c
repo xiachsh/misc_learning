dc08ff31851e9f4320a6fc7a512ce9c79ecdf205
xiachsh@cn.ibm.com <xiachsh@cn.ibm.com>
2016-12-23 01:38:59 -0500

    Add NetTst

---
+++ NetTst/client.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
+
+#include <unistd.h>
+
+#define BLOCKSIZE 512*1024
+
+
+typedef struct testConf {
+	unsigned int threadNum;
+	unsigned int port;
+	unsigned int blockSize;
+	char * server;
+
+} TestConf;
+
+TestConf runConf =  {
+	.threadNum = 16,
+	.port = 5001,
+	.blockSize = BLOCKSIZE,
+	.server = "127.0.0.1",
+};
+
+void usage(int argc,char **argv)
+{
+	fprintf(stderr,"%s usage:\n",argv[0]);
+	fprintf(stderr,"	: -t threadNum default CPU of current hosts\n");
+	fprintf(stderr,"	: -b blockSize default 1M");
+	fprintf(stderr,"	: -s serverName default 127.0.0.1");
+	fprintf(stderr,"	: -p portNum default 5001");
+	exit (1);
+}
+
+
+void parse_args(int argc,char **argv)
+{
+	int c;
+
+	while (( c=getopt(argc,argv,"t:b:s:p:")) != -1 ) {
+		char * v;
+		switch(c) {
+		case 't':
+			 v = optarg;
+			if (!isdigit(v[0])) {
+				printf("%s is not valid for thread num,bye\n",v);
+				usage(argc,argv);			
+			}
+			runConf.threadNum = atoi(v);
+			break;
+		case 'b':
+			 v = optarg;
+			 if (!isdigit(v[0])) {
+                                printf("%s is not valid for thread num,bye\n",v);
+                                usage(argc,argv);
+                        }
+                        runConf.blockSize= atoi(v);
+                        break;
+		case 's':
+			runConf.server = optarg;
+			break;
+		case 'p':
+			  v = optarg;
+                         if (!isdigit(v[0])) {
+                                printf("%s is not valid for thread num,bye\n",v);
+                                usage(argc,argv);
+                        }
+                        runConf.port= atoi(v);
+                        break;
+
+		default:
+			fprintf(stderr,"unknown option %c \n",c);
+			break;
+		}
+	}
+	return;
+}
+
+
+void* worker_fn()
+{
+	
+   int sockfd;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   int * retStat = malloc(sizeof(int) * 1);
+   char * buffer;
+   int sent = 0;
+   *retStat = 1;
+
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      return retStat;
+   }
+   server = gethostbyname(runConf.server);
+   if (server == NULL) {
+      fprintf(stderr,"ERROR, no such host\n");
+      return retStat;
+   }
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   serv_addr.sin_port = htons(runConf.port);
+   if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR connecting");
+      return retStat;
+   }
+   buffer = malloc(sizeof (char) * runConf.blockSize);
+   sent = write(sockfd, buffer, runConf.blockSize);
+   if (sent != runConf.blockSize ) {
+     	 	perror("ERROR writing to socket");
+     		 exit(1);
+   }
+   free(buffer);
+   close(sockfd);	
+
+   *retStat = 0;
+   return retStat;
+
+}
+
+void print_args() 
+{
+	fprintf(stderr,"============================================\n");
+	fprintf(stderr,"Readig to run net test with following option\n");
+	fprintf(stderr,"Thread Num :%d\n",runConf.threadNum);
+	fprintf(stderr,"Port   Num :%d\n",runConf.port);
+	fprintf(stderr,"Server     :%s\n",runConf.server);
+	fprintf(stderr,"Block Size :%d\n",runConf.blockSize);
+
+
+}
+
+int main(int argc, char *argv[]) {
+
+   char *buffer;
+   pthread_t * t_arr;
+   int t_index;
+   parse_args(argc,argv);
+   print_args();
+
+   t_arr = malloc(sizeof(pthread_t) * runConf.threadNum);
+   for (t_index=0;t_index<runConf.threadNum;t_index++){
+	pthread_create(&t_arr[t_index],NULL,worker_fn,NULL);
+   }
+   
+   for (t_index=0;t_index<runConf.threadNum;t_index++){
+	pthread_join(t_arr[t_index],NULL);
+   }
+   
+   
+   free(t_arr);
+   return 0;
+}
+
---
+++ NetTst/server.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define BUFF_SIZE 512*1024
+#define BACKLOG  16
+
+typedef struct testConf {
+	unsigned int port;
+	int buffSize;
+	int backlog;
+} TestConf;
+
+typedef struct _request {
+	int sockfd;
+	struct sockaddr * cli_addr;
+} Request;
+
+#define MAKE_REQ(req,fd,addr)    			 \
+	req = malloc(sizeof(Request));     		 \
+	req->sockfd = fd;				 \
+	req->cli_addr = addr;				 
+
+#define FREE_REQ(req)  					\
+	free(req->cli_addr);				\
+	free(req);						
+
+TestConf runConf =  {
+	.port = 5001,
+	.buffSize = BUFF_SIZE,
+	.backlog = BACKLOG,
+};
+
+void usage(int argc,char **argv)
+{
+	fprintf(stderr,"%s usage:\n",argv[0]);
+	fprintf(stderr,"	: -p portNum default 5001\n");
+	fprintf(stderr,"	: -b buffer size default 512*1024\n");
+	fprintf(stderr,"	: -q backlog backlog size of listen sock\n");
+	exit (1);
+}
+
+
+void parse_args(int argc,char **argv)
+{
+	int c;
+
+	while (( c=getopt(argc,argv,"b:p:")) != -1 ) {
+		char * v;
+		switch(c) {
+		case 'b':
+			 v = optarg;
+                         if (!isdigit(v[0])) {
+                                printf("%s is not valid for thread num,bye\n",v);
+                                usage(argc,argv);
+                        }
+                        runConf.buffSize = atoi(v);
+                        break;
+
+		case 'p':
+			  v = optarg;
+                         if (!isdigit(v[0])) {
+                                printf("%s is not valid for thread num,bye\n",v);
+                                usage(argc,argv);
+                        }
+                        runConf.port= atoi(v);
+                        break;
+		default:
+			fprintf(stderr,"unknown option %c \n",c);
+			break;
+		}
+	}
+	return;
+}
+
+
+void* worker_fn(void * req)
+{
+	int * ret;
+	char *buffer; 
+	int recv;
+	int totalRecv = 0;
+	Request * _req = (Request *) req;
+	buffer = malloc(sizeof(char) * runConf.buffSize);
+        fprintf(stdout,"Processing Request for port....\n");	
+	while ( (recv = read(_req->sockfd,buffer,runConf.buffSize)) > 0 ) 
+		totalRecv += recv;
+	fprintf(stdout,"Get %d bytes in thread id :%ld\n",totalRecv,pthread_self());
+	free(buffer);
+	close(_req->sockfd);
+	return ret;
+}
+
+void print_args() 
+{
+	fprintf(stderr,"============================================\n");
+	fprintf(stderr,"Readig to run net test with following option\n");
+	fprintf(stderr,"Port   Num :%d\n",runConf.port);
+	fprintf(stderr,"Buffer Size :%d\n",runConf.buffSize);
+}
+
+int main(int argc, char *argv[]) {
+
+   char *buffer;
+   int t_index;
+   int sockfd,newsockfd;
+   struct sockaddr_in serv_addr;
+   struct sockaddr_in *cli_addr;
+   socklen_t clilen;
+   parse_args(argc,argv);
+   print_args();
+
+   sockfd = socket(AF_INET,SOCK_STREAM,0); 
+   if (sockfd < 0) {
+	perror("socket");
+	exit(1);
+   }
+
+   bzero( (char*) &serv_addr,sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(runConf.port);
+
+   if (bind(sockfd,(struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+	perror("bind");
+	exit(1);
+   }
+   listen(sockfd,runConf.backlog);
+
+   while (1) {
+	Request *req;
+	pthread_t t;
+	cli_addr = (struct sockaddr_in *) malloc(sizeof(struct sockaddr_in));
+   	clilen = sizeof(cli_addr);
+	newsockfd = accept(sockfd,(struct sockaddr *) cli_addr,&clilen);
+	if (newsockfd < 0) {
+		perror("accept");
+		exit(1);
+	}
+
+	MAKE_REQ(req,newsockfd,(struct sockaddr *)cli_addr);
+	pthread_create(&t,NULL,worker_fn,req);	
+   }
+
+   
+   return 0;
+}
+
